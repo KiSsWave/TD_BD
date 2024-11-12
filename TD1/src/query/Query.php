f89a98122b5589499474c0acadaa5c915931a916
Romain-Pailler <romain.pailler77@gmail.com>
2024-11-12 16:17:44 +0100

    Question 1.1

--- TD1/src/query/Query.php
+++ TD1/src/query/Query.php
@@ -1,1 +1,10 @@
 <?php
+namespace iutnc\hellokant\query;
+
+class Query{
+    private $table_sql;
+    private $fields = '*';
+    private $where = null;
+    private $args = [];
+    private $sql = '';
+}
