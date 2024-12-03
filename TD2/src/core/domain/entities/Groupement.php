829a20437381e05f6201304d4dce0f543f50aed7
jeandidi3u <jeandidier.clement@gmail.com>
2024-12-03 15:27:08 +0100

    entity

--- TD2/src/core/domain/entities/Groupement.php
+++ TD2/src/core/domain/entities/Groupement.php
@@ -6,6 +6,8 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\Common\Collections\Collection;
@@ -34,6 +36,12 @@ class Groupement
 
     #[OneToMany(mappedBy: "groupement", targetEntity: Praticien::class)]
     private Collection $praticiens;
+
+    #[ManyToOne(targetEntity: Type_Groupement::class)]
+    #[JoinColumn(name: "type__id", referencedColumnName: "id", nullable: false)]
+    private Type_Groupement $type_id;
+
+
 
 }
 
