c8f212492a36cb5fb23a9e5852636ec400ddd2c0
jeandidi3u <jeandidier.clement@gmail.com>
2024-12-03 15:15:12 +0100

    entity

--- TD2/src/core/domain/entities/Groupement.php
+++ TD2/src/core/domain/entities/Groupement.php
@@ -6,7 +6,9 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\Table;
+use Doctrine\Common\Collections\Collection;
 
 #[Entity]
 #[Table(name: "groupement")]
@@ -29,6 +31,9 @@ class Groupement
 
     #[Column(type: "string", length: 20)]
     private string $telephone;
+
+    #[OneToMany(mappedBy: "groupement", targetEntity: Praticien::class)]
+    private Collection $praticiens;
 
 }
 
